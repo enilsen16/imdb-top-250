bb4a3b3c95009f92e13898984c6a6d790f197ad8
Erik Nilsen <enilsen16@live.com>
2015-09-13 15:56:19 -0700

    Include the date in top_250.txt

--- top250.rb
+++ top250.rb
@@ -1,6 +1,8 @@
 require "nokogiri"
 require 'open-uri'
 
+# Sorted by ranking
 doc = Nokogiri::HTML(open('http://akas.imdb.com/chart/top/'))
 
+puts "Top 250 Movies for: #{Time.now}\n\n"
 puts doc.xpath('//td//a').search("a[@href^='/title/tt']" && "a[@title]").children
